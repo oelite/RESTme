ff0c4a6eac6b80337858e58e04bef448a6685b2f
Lida Weng <lida.weng@webcider.com>
2018-08-09 18:00:53 +0100

    add htmlutlitypack parsing

--- OElite.Restme.Utils/DateTimeUtils.cs
+++ OElite.Restme.Utils/DateTimeUtils.cs
@@ -78,5 +78,164 @@ namespace OElite
         {
             return TimeZoneInfo.GetSystemTimeZones().ToList();
         }
+
+        public static int TotalMonthsBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Months;
+
+        public static int TotalDaysBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Days;
+
+        public static int TotalYearsBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Years;
+
+        public static int TotalHoursBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Hours;
+
+        public static int TotalMinutesBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Minutes;
+
+        public static int TotalSecondsBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Seconds;
+
+        public static int TotalMillisecondsBetween(DateTime time1, DateTime time2) =>
+            DateTimeSpan.CompareDates(time1, time2).Milliseconds;
+
+        public static int TotalWeeksBetween(DateTime time1, DateTime time2) =>
+            (int) Math.Ceiling(DateTimeSpan.CompareDates(time1, time2).Days / 7d);
+    }
+
+    public struct DateTimeSpan
+    {
+        private readonly int years;
+        private readonly int months;
+        private readonly int days;
+        private readonly int hours;
+        private readonly int minutes;
+        private readonly int seconds;
+        private readonly int milliseconds;
+
+        public DateTimeSpan(int years, int months, int days, int hours, int minutes, int seconds, int milliseconds)
+        {
+            this.years = years;
+            this.months = months;
+            this.days = days;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.milliseconds = milliseconds;
+        }
+
+        public int Years
+        {
+            get { return years; }
+        }
+
+        public int Months
+        {
+            get { return months; }
+        }
+
+        public int Days
+        {
+            get { return days; }
+        }
+
+        public int Hours
+        {
+            get { return hours; }
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+        }
+
+        public int Seconds
+        {
+            get { return seconds; }
+        }
+
+        public int Milliseconds
+        {
+            get { return milliseconds; }
+        }
+
+        enum Phase
+        {
+            Years,
+            Months,
+            Days,
+            Done
+        }
+
+        public static DateTimeSpan CompareDates(DateTime date1, DateTime date2)
+        {
+            if (date2 < date1)
+            {
+                var sub = date1;
+                date1 = date2;
+                date2 = sub;
+            }
+
+            DateTime current = date1;
+            int years = 0;
+            int months = 0;
+            int days = 0;
+
+            Phase phase = Phase.Years;
+            DateTimeSpan span = new DateTimeSpan();
+            int officialDay = current.Day;
+
+            while (phase != Phase.Done)
+            {
+                switch (phase)
+                {
+                    case Phase.Years:
+                        if (current.AddYears(years + 1) > date2)
+                        {
+                            phase = Phase.Months;
+                            current = current.AddYears(years);
+                        }
+                        else
+                        {
+                            years++;
+                        }
+
+                        break;
+                    case Phase.Months:
+                        if (current.AddMonths(months + 1) > date2)
+                        {
+                            phase = Phase.Days;
+                            current = current.AddMonths(months);
+                            if (current.Day < officialDay &&
+                                officialDay <= DateTime.DaysInMonth(current.Year, current.Month))
+                                current = current.AddDays(officialDay - current.Day);
+                        }
+                        else
+                        {
+                            months++;
+                        }
+
+                        break;
+                    case Phase.Days:
+                        if (current.AddDays(days + 1) > date2)
+                        {
+                            current = current.AddDays(days);
+                            var timespan = date2 - current;
+                            span = new DateTimeSpan(years, months, days, timespan.Hours, timespan.Minutes,
+                                timespan.Seconds, timespan.Milliseconds);
+                            phase = Phase.Done;
+                        }
+                        else
+                        {
+                            days++;
+                        }
+
+                        break;
+                }
+            }
+
+            return span;
+        }
     }
 }
