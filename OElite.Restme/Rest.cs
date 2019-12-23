de7dcc464662b1363a8e3b4c6921ece5d4ae4a75
Lida Weng <lida.weng@webcider.com>
2019-12-23 11:04:52 +0000

    make bearer optional

--- OElite.Restme/Rest.cs
+++ OElite.Restme/Rest.cs
@@ -120,9 +120,9 @@ namespace OElite
                 _headers.Add(header, new List<string> {value});
         }
 
-        public void AddBearerToken(string token)
+        public void AddBearerToken(string token, bool addBearerPrefix = true)
         {
-            AddHeader("Authorization", $"Bearer {token}");
+            AddHeader("Authorization", $"{(addBearerPrefix ? "Bearer " : "")}{token}");
         }
 
         public T Request<T>(HttpMethod method, string relativeUrlPath = null)
