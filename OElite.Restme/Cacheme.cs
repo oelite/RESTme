603e863b9da66d7686ec4757a1267be0038b1fb5
Lida Weng <lida.weng@webcider.com>
2025-05-03 08:27:49 +0800

    update cacheme to take object as query and use md5 to encrypt into unique key

--- OElite.Restme/Cacheme.cs
+++ OElite.Restme/Cacheme.cs
@@ -101,4 +101,38 @@ public static class RestmeCacheExtensions
 
         return await refreshAction.Invoke();
     }
+
+
+    public static Task<T> FindmeAsync<T>(this Rest rest, object queryObject, bool returnExpired = false,
+        bool returnInGrace = true,
+        Func<T, Task<bool>> additionalValidation = null,
+        Func<Task<T>> refreshAction = null) where T : class
+    {
+        if (queryObject == null) throw new ArgumentNullException(nameof(queryObject));
+        var json = queryObject.JsonSerialize();
+        if (json.IsNullOrEmpty()) throw new OEliteException("No valid query object identified");
+        var md5 = EncryptHelper.MD5Encrypt(queryObject.JsonSerialize());
+        return rest.FindmeAsync(md5, returnExpired, returnInGrace, additionalValidation, refreshAction);
+    }
+
+    public static Task<ResponseMessage> CachemeAsync(this Rest rest, object queryObject, object data,
+        int expiryInSeconds = -1,
+        int graceInSeconds = -1)
+    {
+        if (queryObject == null) throw new ArgumentNullException(nameof(queryObject));
+        var json = queryObject.JsonSerialize();
+        if (json.IsNullOrEmpty()) throw new OEliteException("No valid query object identified");
+        var md5 = EncryptHelper.MD5Encrypt(queryObject.JsonSerialize());
+        return rest.CachemeAsync(md5, data, expiryInSeconds, graceInSeconds);
+    }
+
+    public static Task<bool> ExpiremeAsync(this Rest rest, object queryObject, bool invalidateGracePeriod = true)
+    {
+        if (queryObject == null) throw new ArgumentNullException(nameof(queryObject));
+        var json = queryObject.JsonSerialize();
+        if (json.IsNullOrEmpty()) throw new OEliteException("No valid query object identified");
+        var md5 = EncryptHelper.MD5Encrypt(queryObject.JsonSerialize());
+
+        return rest.ExpiremeAsync(md5, invalidateGracePeriod);
+    }
 }
